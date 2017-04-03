2c74a50aad5ddfdc1519bfc0df540855da5dfe8d
MrIndi <t33th4n@gmail.com>
2017-04-03 17:42:34 +0200

    Minor change

--- backter.sh
+++ backter.sh
@@ -10,7 +10,6 @@
 # Directory:/home/userx/workspace:.+:JavaWorkspaceB
 # Directory:/var/log:.+:SysLogs
 
-
 cfg="backterHouse.cfg"
 sdups="Y"
 sbacks="Y"
@@ -27,8 +26,8 @@ if test $# -eq 0; then
 	echo "Usage: $0 [-c] COMMAND"
 	echo "COMMAND:"
 	echo "   -c file  - Specifies the config file"
-	echo "	-id      - Hide messages for already backed up files"
-	echo "	-ib      - Hide backup messages"
+	echo "   -id      - Hide messages for already backed up files"
+	echo "   -ib      - Hide backup messages"
 	echo "   -im      - Hide messages for missing files at verify"
 	echo "   backup   - Creates backup according the configuration"
 	echo "   compare  - Compares the content of the backed up files and their existing counterparts"
@@ -114,7 +113,7 @@ if test "$1" = "backup"; then
 		fi
 	done
 fi
-if test "$1" = "verify"; then
+if test "$1" = "compare"; then
 	echo "Verify selected..."
 	cat "$cfg" | while read cl; do
 		com=`echo "$cl" | cut -d: -f1`
